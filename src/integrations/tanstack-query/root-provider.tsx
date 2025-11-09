e2632fe9f3507d7dfba6681a19b125c88d9e203b
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2025-11-09 21:36:38 +0200

    chore: query cache
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- src/integrations/tanstack-query/root-provider.tsx
+++ src/integrations/tanstack-query/root-provider.tsx
@@ -1,20 +1,30 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export function getContext() {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 365 * 24 * 60 * 60 * 1000,
+        gcTime: 365 * 24 * 60 * 60 * 1000,
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+      },
+    },
+  });
   return {
     queryClient,
-  }
+  };
 }
 
 export function Provider({
   children,
   queryClient,
 }: {
-  children: React.ReactNode
-  queryClient: QueryClient
+  children: React.ReactNode;
+  queryClient: QueryClient;
 }) {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
+  );
 }
