881c73cec6c62cc5eeae68b97e4dcf1a9197a69d
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2025-11-09 22:58:34 +0200

    chore: back to nitro v2...
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-// import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
-import { nitro } from "nitro/vite";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
+// import { nitro } from "nitro/vite";
 
 const config = defineConfig({
   plugins: [
@@ -13,10 +13,10 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
-    nitro(),
-    // nitroV2Plugin({
-    //   compatibilityDate: "2025-11-09",
-    // }),
+    // nitro(),
+    nitroV2Plugin({
+      compatibilityDate: "2025-11-09",
+    }),
     viteReact({
       babel: {
         plugins: ["babel-plugin-react-compiler"],
